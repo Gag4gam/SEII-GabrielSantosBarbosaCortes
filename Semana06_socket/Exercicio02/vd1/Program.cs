6875ebb2bf972f1e0c34be325dbfdadbc64fd8d8
Gabriel Santos <gabriel__cortes@hotmail.com>
2024-10-20 18:51:54 -0300

    até semana 08

---
+++ Semana06_socket/Exercicio02/vd1/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Reflection.Metadata;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vd1
+{
+    class Program
+    {
+        public class SyncSocketClient
+        {
+           public static void StartClient()
+           {
+                byte[] bytes = new byte[1024];
+                try
+                {
+                    var hostName = Dns.GetHostName();
+                    IPHostEntry iphost = Dns.GetHostEntry(hostName);
+                    Console.WriteLine($"Host: {hostName})");
+                    IPAddress ip = iphost.AddressList[0];
+                    IPEndPoint remoteEP = new IPEndPoint(ip, 43665);
+                    
+                    Socket sender = new Socket(ip.AddressFamily, SocketType.Stream,ProtocolType.Tcp);
+                    try
+                    {
+                        sender.Connect(remoteEP);
+                        Console.WriteLine("socket connected");
+                        sender.RemoteEndPoint.ToString();
+                        byte[] msg = Encoding.ASCII.GetBytes("This is just a test");
+                        int byteSent = sender.Send(msg);
+                        int byteRec = sender.Receive(bytes);
+                        Console.WriteLine($"Echoed test {Encoding.ASCII.GetString(bytes,0,byteRec)}");
+
+                        ///relase
+                        sender.Shutdown(SocketShutdown.Both);
+                        sender.Close();
+                    }
+                    catch (ArgumentException e)
+                    {
+                        Console.WriteLine(e.Message);
+                        throw;
+                    }
+                    catch(SocketException e){
+                        Console.WriteLine(e.Message);
+                    }
+                    catch(Exception e){
+                        Console.WriteLine(e.Message);
+                    }
+
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+           }
+        }
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("press any key to cont....");
+            Console.ReadLine();
+            SyncSocketClient.StartClient();
+            Console.ReadLine();
+
+        }
+
+
+    }
+}
---
+++ Semana06_socket/Exercicio03/vd2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Net;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Sockets;
+
+namespace vd2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            sck = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPEndPoint LocalEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 1234);
+            try
+            {
+                sck.Connect(LocalEndPoint);
+            }
+            catch
+            {
+                Console.Write("Unable to connect to remote point");
+                Main(args);
+            }
+            Console.Write("Enter Text: ");
+            string text = Console.ReadLine();
+            byte[] data = Encoding.ASCII.GetBytes(text);
+
+            sck.Send(data);
+            Console.Write("Data sent!\r\n");
+            Console.Write("press any key to continue....");
+            Console.Read();
+            sck.Close();
+        }
+    }
+}
